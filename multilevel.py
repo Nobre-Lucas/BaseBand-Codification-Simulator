b679eb51fbf716bc78cc220d1b55689de467438a
Helder Matos <mtshelder@gmail.com>
2021-04-17 19:07:42 -0300

    Rename AMI method and add Pseudoternary method

--- multilevel.py
+++ multilevel.py
@@ -3,48 +3,74 @@ import matplotlib.pyplot as plt
 
 class Encoding:
 
-    def __init__(self, word):
-        self.word = word
-        self.code = []
+    def __init__(self, bits):
+        self.bits = bits
+        self.codes = {"NRZI": [],
+                      "HDB3": [],
+                      "Manchester": [],
+                      "AMI": [],
+                      "Pseudoternary": []}
 
-    def display(self):
-        print(self.word)
+    def get_bits(self):
+        return self.bits
 
-    def displayCode(self):
-        print(self.code)
+    def get_code(self, scheme: str) -> list:
+        return self.codes[scheme]
 
-    def multilevel(self):
-        self.code = []
+    def ami(self) -> list:
+        code = []
 
         level = True
 
-        for bit in self.word:
+        for bit in self.bits:
             if bit == "0":
-                self.code.append(0)
+                code.append(0)
             else:
                 if level is True:
-                    self.code.append(1)
+                    code.append(1)
                 else:
-                    self.code.append(-1)
+                    code.append(-1)
                 level = not level
 
-        return self.code
+        return code
+
+    def pseudoternary(self) -> list:
+        code = []
+        
+        level = True
+        
+        for bit in self.bits:
+            if bit == "0":
+                if level is True:
+                    code.append(1)
+                else:
+                    code.append(-1)
+                level = not level
+            else:
+                code.append(0)
+        
+        return code
+    
+    def encode(self):
+        self.codes["AMI"] = self.ami()
+        self.codes["Pseudoternary"] = self.pseudoternary()
 
     def plot(self):
-        x = [i for i in range(0, len(self.code))]
-        y = self.code
+        x = [i for i in range(0, len(self.bits))]
         plt.figure(figsize=(20, 5))
         plt.grid()
-        plt.axis([-1, len(self.word), -1.1, 1.1])
+        plt.axis([-1, len(self.bits), -1.1, 1.1])
         plt.xlabel("Bits ao longo do tempo")
         plt.ylabel("Níveis")
-        plt.plot(x, y, label="Multilevel", color='red', drawstyle='steps-pre')
+        plt.plot(x, self.codes["AMI"], label="AMI", color='r', drawstyle='steps-post')
+        plt.plot(x, self.codes["Pseudoternary"], label="Multilevel", color='b', drawstyle='steps-post')
         plt.legend()
         plt.show()
-
+        
 
 bits = Encoding("01001100011")
-bits.display()
-bits.multilevel()
-bits.displayCode()
+bits.encode()
+print(bits.get_bits())
+print(bits.get_code("AMI"))
+print(bits.get_code("Pseudoternary"))
 bits.plot()
