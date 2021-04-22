2c646afe6f6acc5ce53221502320f475f90498a1
Nobre Lucas <lucasbnobre18@gmail.com>
2021-04-21 23:08:41 -0300

    added nrzi encoding

--- encoding.py
+++ encoding.py
@@ -13,11 +13,11 @@ class Encoding:
 
         if valid = True:
             self.codes = {"Bits": [int(bit) for bit in bits],
-                        # "NRZI": [],
+                          "NRZI": [],
                         # "HDB3": [],
                         # "Manchester": [],
-                        "AMI": [],
-                        "Pseudoternary": []}
+                          "AMI": [],
+                          "Pseudoternary": []}
         else:
             raise ValueError ("Os dados só podem possuir bits 0 ou 1")
 
@@ -26,6 +26,35 @@ class Encoding:
 
     def get_code(self, scheme: str) -> list:
         return self.codes[scheme]
+
+    def nrzi(self) -> list:
+        code = [1, 1]
+        timestamp = [0]
+
+        counter = 0
+        states = (1, -1)
+        state = 0
+
+        for i in self.codes["Bits"]:
+
+            counter += 1
+
+            if i == 1:
+                state = 1 if (state == 0) else 0
+                code.append(states[state])
+                code.append(states[state])
+
+                timestamp.append(counter)
+                timestamp.append(counter)
+
+            elif i == 0:
+                code.append(states[state])
+                timestamp.append(counter)
+
+        code.pop()
+
+        return code
+
 
     def ami(self) -> list:
         code = []
