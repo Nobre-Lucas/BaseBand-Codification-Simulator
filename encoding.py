fb36cd7a90295a7619cd5b8403a9fad38b7acb3e
Helder Matos <mtshelder@gmail.com>
2021-04-23 21:23:46 -0300

    Adjust plot for 2B1Q

--- encoding.py
+++ encoding.py
@@ -61,40 +61,40 @@ class Encoding:
                            "01": (+3, -3),
                            "10": (-1, +1),
                            "11": (-3, +3)}
-        
+
         o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
         o_bits += [str(i) for i in self.codes["Bits"]]
-        
+
         dibits = [[o_bits[i], o_bits[i+1]] for i in range(0, len(o_bits), 2)]
-        
+
         code = []
         timestamp = [0]
-        
+
         previous_level = True # T if (previous > 0 or initial) else F
         counter = 0
-        
+
         for dibit in dibits:
-            
+
             counter += 1
-            
+
             if previous_level is True:
                 current_level = translate_table["".join(dibit)][0]
             else:
                 current_level = translate_table["".join(dibit)][1]
-                
+
             code.append(current_level)
             code.append(current_level)
-                
+
             timestamp.append(counter)
             timestamp.append(counter)
-            
+
             previous_level = True if current_level > 0 else False
-            
+
         timestamp.pop()
-        
+
         return code, timestamp
-    
-    
+
+
     def encode(self):
         self.codes["NRZI"] = self.nrzi()[0]
         self.codes["2B1Q"] = self.tboq()[0]
@@ -102,6 +102,8 @@ class Encoding:
 
     def plot(self, scheme: str):
         
+        fig, axs = plt.subplots(1)
+
         x_axis = []
         y_axis = []
 
@@ -112,10 +114,20 @@ class Encoding:
             bit_code = [str(i) for i in self.codes["Bits"]]
             bit_code.insert(0, '')
             plt.yticks([-1, 0, 1], ['-1', '', '1'])
-            
+
         elif scheme == "2B1Q":
             x_axis = self.tboq()[1]
             y_axis = self.codes["2B1Q"]
+            
+            plt.ylim(-3.1, 3.1)
+            
+            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
+            
+            o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
+            o_bits += [str(i) for i in self.codes["Bits"]]
+
+            bit_code = ["".join([o_bits[i], o_bits[i+1]])
+                        for i in range(0, len(o_bits), 2)]
             
         else:
             raise ValueError("Available schemes: NRZI, HDB3, Manchester, 2B1Q")
@@ -124,7 +136,7 @@ class Encoding:
         plt.plot(x_axis, y_axis, 'black', linewidth=2)
         plt.grid()
         plt.show()
-        
+
 
 bits = Encoding("10110010")
 bits.encode()
@@ -132,4 +144,4 @@ print("Bits:", bits.get_bits())
 print("NRZI:", bits.get_code("NRZI"))
 print("2B1Q:", bits.get_code("2B1Q"))
 
-bits.plot("NRZI")
+bits.plot("2B1Q")
