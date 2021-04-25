2ab5739b78aec2b9e2c9cd6391c169d0ce0aa836
Helder Matos <mtshelder@gmail.com>
2021-04-25 18:00:38 -0300

    Placing xticks in each scheme

--- encoding.py
+++ encoding.py
@@ -221,6 +221,8 @@ class Encoding:
 
             bit_code = [str(i) for i in self.codes["Bits"]]
             bit_code.insert(0, '')
+            
+            plt.xticks(np.arange(len(x_axis)), bit_code)
             plt.yticks([-1, 0, 1], ['-1', '', '1'])
 
         elif scheme == "HDB3":
@@ -233,18 +235,19 @@ class Encoding:
             
             plt.ylim(-3.1, 3.1)
             
-            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
             
             o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
             o_bits += [str(i) for i in self.codes["Bits"]]
 
-            bit_code = ["".join([o_bits[i], o_bits[i+1]])
-                        for i in range(0, len(o_bits), 2)]
+            bit_code = ["".join([o_bits[i], o_bits[i+1]]) for i in range(0, len(o_bits), 2)]
+            bit_code = ['' if i % 2 == 0 else bit_code[i//2] for i in range(len(bit_code)*2)] + ['']
+                                    
+            plt.xticks(np.arange(len(x_axis))/2, bit_code)
+            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
             
         else:
             raise ValueError("Available schemes: NRZI, HDB3, Manchester, 2B1Q")
 
-        plt.xticks(np.arange(len(x_axis)), bit_code)
         plt.plot(x_axis, y_axis, 'black', linewidth=2)
         plt.grid()
         plt.show()
@@ -255,4 +258,4 @@ print("Bits:", bits.get_bits())
 print("NRZI:", bits.get_code("NRZI"))
 print("2B1Q:", bits.get_code("2B1Q"))
 
-bits.plot("HDB3")
+bits.plot("2B1Q")
