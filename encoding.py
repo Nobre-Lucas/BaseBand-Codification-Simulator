64e4f7f953b8ffb1d3e18e1324ccddbfbb6d17c4
Helder Matos <mtshelder@gmail.com>
2021-04-25 17:43:05 -0300

    Fixing xticks for 2B1Q

--- encoding.py
+++ encoding.py
@@ -121,24 +121,26 @@ class Encoding:
             
             plt.ylim(-3.1, 3.1)
             
-            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
             
             o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
             o_bits += [str(i) for i in self.codes["Bits"]]
 
-            bit_code = ["".join([o_bits[i], o_bits[i+1]])
-                        for i in range(0, len(o_bits), 2)]
+            bit_code = ["".join([o_bits[i], o_bits[i+1]]) for i in range(0, len(o_bits), 2)]
+            bit_code = ['' if i % 2 == 0 else bit_code[i//2] for i in range(len(bit_code)*2)] + ['']
+                                    
+            plt.xticks(np.arange(len(x_axis))/2, bit_code)
+            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
             
         else:
             raise ValueError("Available schemes: NRZI, HDB3, Manchester, 2B1Q")
 
-        plt.xticks(np.arange(len(x_axis)), bit_code)
+        
         plt.plot(x_axis, y_axis, 'black', linewidth=2)
         plt.grid()
         plt.show()
 
 
-bits = Encoding("10110010")
+bits = Encoding("101100100111")
 bits.encode()
 print("Bits:", bits.get_bits())
 print("NRZI:", bits.get_code("NRZI"))
