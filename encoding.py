a6ef2726ec3413b29636e2f73a391c07782e69b2
Par-zy <j.davi4242@gmail.com>
2021-04-25 22:08:47 -0300

    add multilevel encoding

--- encoding.py
+++ encoding.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class Encoding:
 
@@ -14,10 +14,10 @@ class Encoding:
             self.codes = {"Bits": [int(bit) for bit in bits],
                           "NRZI": [],
                         # "HDB3": [],
-                        # "Manchester": [],
+                          "Manchester": [],
                           "2B1Q": []}
         else:
-            raise ValueError ("Os dados só podem possuir bits 0 ou 1")
+            raise ValueError ("Available entry: characters 1 or 0")
 
     def get_bits(self) -> list:
         return self.codes["Bits"]
@@ -56,74 +56,168 @@ class Encoding:
         return code, timestamp
 
 
+    def manchester(self) -> list:
+      code = [1 if self.codes["Bits"][0] == 0 else 0]
+      timestamp = []
+
+      counter = 0
+      states = (1, 0)
+      counter2 = 0
+
+      for i in self.codes["Bits"]: #10110010
+        if i == 1:
+          code.append(states[1])
+          timestamp.append(counter)
+          counter += 0.5
+           
+          code.append(states[0])
+          timestamp.append(counter)
+
+          code.append(states[0])
+          timestamp.append(counter)
+          counter += 0.5
+
+        elif i == 0:
+          code.append(states[0])
+          timestamp.append(counter)  
+          counter += 0.5
+
+          code.append(states[1])
+          timestamp.append(counter)
+
+          code.append(states[1])
+          timestamp.append(counter)
+          counter += 0.5
+
+            
+        if counter2 < len(self.codes["Bits"])-1:
+          if (self.codes["Bits"][counter2+1] == 0 and self.codes["Bits"][counter2] == 0):
+            code.append(states[0])
+            timestamp.append(counter)
+            print("teste")
+
+          if (self.codes["Bits"][counter2+1] == 1 and self.codes["Bits"][counter2] == 1):
+            code.append(states[1])
+            timestamp.append(counter)
+            print("teste2")
+        counter2 += 1
+      
+      if (self.codes["Bits"][-1] == 1):
+        
+        code.append(states[0])
+        timestamp.append(counter)  
+
+      if (self.codes["Bits"][-1] == 0):
+    
+        code.append(states[1])
+        timestamp.append(counter)
+
+      code.pop() 
+
+      return code, timestamp 
+
+
     def tboq(self) -> list:
         translate_table = {"00": (+1, -1),
                            "01": (+3, -3),
                            "10": (-1, +1),
                            "11": (-3, +3)}
-        
+
         o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
         o_bits += [str(i) for i in self.codes["Bits"]]
-        
+
         dibits = [[o_bits[i], o_bits[i+1]] for i in range(0, len(o_bits), 2)]
-        
+
         code = []
         timestamp = [0]
-        
+
         previous_level = True # T if (previous > 0 or initial) else F
         counter = 0
-        
+
         for dibit in dibits:
-            
+
             counter += 1
-            
+
             if previous_level is True:
-                actual_level = translate_table["".join(dibit)][0]
+                current_level = translate_table["".join(dibit)][0]
             else:
-                actual_level = translate_table["".join(dibit)][1]
-                
-            code.append(actual_level)
-            code.append(actual_level)
-                
+                current_level = translate_table["".join(dibit)][1]
+
+            code.append(current_level)
+            code.append(current_level)
+
             timestamp.append(counter)
             timestamp.append(counter)
-            
-            previous_level = True if actual_level > 0 else False
-            
+
+            previous_level = True if current_level > 0 else False
+
         timestamp.pop()
-        
+
         return code, timestamp
-    
-    
+
+
     def encode(self):
         self.codes["NRZI"] = self.nrzi()[0]
         self.codes["2B1Q"] = self.tboq()[0]
+        self.codes["Manchester"] = self.manchester()[0]
 
 
     def plot(self, scheme: str):
         
+        fig, axs = plt.subplots(1)
+
         x_axis = []
         y_axis = []
 
         if scheme == "NRZI":
             x_axis = self.nrzi()[1]
             y_axis = self.codes["NRZI"]
-            
+
+            bit_code = [str(i) for i in self.codes["Bits"]]
+            bit_code.insert(0, '')
+            plt.yticks([-1, 0, 1], ['-1', '', '1'])
+
         elif scheme == "2B1Q":
             x_axis = self.tboq()[1]
             y_axis = self.codes["2B1Q"]
             
+            plt.ylim(-3.1, 3.1)
+            
+            plt.yticks([-3, -1, 0, 1, 3], ['-3', '-1', '', '1', '3'])
+            
+            o_bits = [0] if (len(self.codes["Bits"]) % 2 != 0) else []
+            o_bits += [str(i) for i in self.codes["Bits"]]
+
+            bit_code = ["".join([o_bits[i], o_bits[i+1]])
+                        for i in range(0, len(o_bits), 2)]
+
+        elif scheme == "Manchester":
+            x_axis = self.manchester()[1]
+            y_axis = self.codes["Manchester"]
+
+            bit_code = []
+            for i in self.codes["Bits"]:
+              bit_code.append('')
+              bit_code.append(i)
+
+            plt.yticks([0, 1], [ '0', '1'])
+            plt.xticks(np.arange(0, len(x_axis), 0.5), bit_code)
+
+
+            
         else:
             raise ValueError("Available schemes: NRZI, HDB3, Manchester, 2B1Q")
 
-        plt.plot(x_axis, y_axis)
+        plt.plot(x_axis, y_axis, 'black', linewidth=2)
+        plt.grid()
         plt.show()
-        
 
-bits = Encoding("110011001011")
+
+bits = Encoding("10011001")
 bits.encode()
 print("Bits:", bits.get_bits())
 print("NRZI:", bits.get_code("NRZI"))
+print("Manchester:", bits.get_code("Manchester"))
 print("2B1Q:", bits.get_code("2B1Q"))
 
-bits.plot("2B1Q")
+bits.plot("Manchester")
--- multilevel.py
+++ multilevel.py
@@ -1,84 +1,108 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-
-class Encoding:
-
-    def __init__(self, bits):
-        self.bits = bits
-        self.codes = {"NRZI": [],
-                      "HDB3": [],
-                      "Manchester": [],
-                      "AMI": [],
-                      "Pseudoternary": []}
-
-    def get_bits(self):
-        return self.bits
-
-    def get_code(self, scheme: str) -> list:
-        return self.codes[scheme]
-
-    def ami(self) -> list:
-        code = []
-
-        level = True
-
-        for bit in self.bits:
-            if bit == "0":
-                code.append(0)
-            else:
-                if level is True:
-                    code.append(1)
-                else:
-                    code.append(-1)
-                level = not level
-
-        return code
-
-    def pseudoternary(self) -> list:
-        code = []
-        
-        level = True
-        
-        for bit in self.bits:
-            if bit == "0":
-                if level is True:
-                    code.append(1)
-                else:
-                    code.append(-1)
-                level = not level
-            else:
-                code.append(0)
-        
-        return code
+class Manchester:
     
-    def encode(self):
-        self.codes["AMI"] = self.ami()
-        self.codes["Pseudoternary"] = self.pseudoternary()
-
-    def plot(self):
-        x = [i for i in range(len(self.bits))]
+    def __init__(self, data):
         
-        fig, axs = plt.subplots(3,figsize=(10,10))
-        fig.tight_layout(pad=3.0)
-        
-        major_ticks = np.arange(0, len(self.bits), 1)
-        
-        for y, ax in zip(self.codes.values(), fig.axes):
-            if len(y) > 0:
-                ax.set_xticks(major_ticks)
-                ax.set_yticks(major_ticks)
-                ax.grid(which='both')
+        validation = True
+        for i in data:
+            if(i != '0') and (i != '1'):
+                validation = False
                 
-                ax.plot(x,y,color='red',drawstyle="steps-post")
-                ax.set_title("AMI")
-        
-        plt.show()
-        
+        if validation:
+            self.data = data
+        else:
+            raise ValueError ("Os dados só podem possuir bits 0 ou 1")
 
-bits = Encoding("01001100011")
-bits.encode()
-print(bits.get_bits())
-print(bits.get_code("AMI"))
-print(bits.get_code("Pseudoternary"))
-bits.plot()
+        self.data =  [int(bit) for bit in data]
+        ternary = 1 if self.data[0] == 0 else 0
+        self.mensagem = [1 if self.data[0] == 0 else 0]
+        self.timestamp = []
+    
+        
+    def convert_data(self):
+
+            aux = []
+    
+            for i in self.data:
+                aux.append(int(i))
+    
+            self.data = aux
+            
+    def transmit_message(self):
+        self.convert_data()
+            
+        estados = (1, 0)
+        counter = 0
+        contador2 = 0
+        for i in self.data: #10110010
+            
+            if i == 1:
+                self.mensagem.append(estados[1])
+                self.timestamp.append(counter)
+                counter += 0.5
+                #print(estados[i])
+                #print("Esse é o counter após transicionar no bit 1: ", counter)
+           
+
+                self.mensagem.append(estados[0])
+                self.timestamp.append(counter)
+
+                self.mensagem.append(estados[0])
+                self.timestamp.append(counter)
+                counter += 0.5
+                #print(estados[i])
+                #print("Esse é o counter após passar no bit 1: ", counter)
+
+            elif i == 0:
+
+                self.mensagem.append(estados[0])
+                self.timestamp.append(counter)  
+                counter += 0.5
+
+                #print(estados[i])
+                #print("Esse é o counter após transicionar no bit 0: ", counter)
+
+                self.mensagem.append(estados[1])
+                self.timestamp.append(counter)
+
+                self.mensagem.append(estados[1])
+                self.timestamp.append(counter)
+                counter += 0.5
+
+                #print(estados[i])
+                #print("Esse é o counter após passar no bit 0: ", counter)
+            
+            if contador2 < len(self.data)-1:
+                if (self.data[contador2+1] == 0 and self.data[contador2] == 0):
+                    self.mensagem.append(estados[0])
+                    self.timestamp.append(counter)
+                    print("teste")
+
+                if (self.data[contador2+1] == 1 and self.data[contador2] == 1):
+                    self.mensagem.append(estados[1])
+                    self.timestamp.append(counter)
+                    print("teste2")
+            contador2 += 1
+
+        if(self.data[-1] == 1):
+    
+            self.mensagem.append(estados[0])
+            self.timestamp.append(counter)  
+
+        if(self.data[-1] == 0):
+    
+            self.mensagem.append(estados[1])
+            self.timestamp.append(counter)  
+
+        print(self.mensagem)
+        print(self.timestamp)
+
+        self.mensagem.pop()
+
+        plt.plot(self.timestamp, self.mensagem)
+        plt.show()
+
+
+inputbits = Manchester("10011001")
+inputbits.transmit_message()
